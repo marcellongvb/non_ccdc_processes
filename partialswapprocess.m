8bfdf6cb630f0fcf065571c3566c3b054714229b
Marcello <mnerygvb@gmail.com>
2020-09-09 15:28:13 -0300

    Included calculations without removing Aux_o

--- partialswapprocess.m
+++ partialswapprocess.m
@@ -9,7 +9,7 @@ SWAP = [1 0 0 0;
         0 1 0 0;
         0 0 0 1];
 clear i
-Decoder = 1/sqrt(2) * (eye(4) + i * SWAP)
+Decoder = 1/sqrt(2) * (eye(4) + i* SWAP)
 
 decoder_choi = zeros(16,16);
 for i = 0:1
@@ -23,20 +23,42 @@ for i = 0:1
 end
 clear i
 
-decoder_choi = PartialTrace(decoder_choi,4,[2 2 2 2]);
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% Uncomment this block for calculation without removing Auxo
+
 % decoder_choi Hilbert spaces: Aux Ao/ Bi
 
 % Input state: Ai Aux
 phiplus = 1/sqrt(2) * (kron(ket(0),ket(0)) + kron(ket(1), ket(1)));
 phiplus = phiplus * phiplus'
 
-term_1 = kron(PartialTranspose(phiplus,2,[2 2]), eye(4)); % Hilbert spaces: Ai Aux Ao Bi
-term_2 = kron(eye(2), decoder_choi); % Hilbert spaces: Ai Aux Ao Bi
+term_1 = kron(PartialTranspose(phiplus,2,[2 2]), eye(8)); % Hilbert spaces: Ai Aux Ao Bi Auxo
+term_2 = kron(eye(2), decoder_choi); % Hilbert spaces: Ai Aux Ao Bi Auxo
 
-W = PartialTrace(term_1 * term_2, 2,[2 2 2 2]);
+% Removing Aux:
+W = PartialTrace(term_1 * term_2, 2,[2 2 2 4]);
 
-[J,S ] = ccdc_complete([2 2 2 1 1 1], 'GR', 'primal', W);
+[J,S ] = ccdc_complete([2 2 4 1 1 1], 'GR', 'primal', W);
 
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+% % Uncomment this block for calculation with removed Auxo
+% % decoder_choi Hilbert spaces: Aux Ao/ Bi 
+% decoder_choi = PartialTrace(decoder_choi,4,[2 2 2 2]);
+% 
+% 
+% Input state: Ai Aux
+% phiplus = 1/sqrt(2) * (kron(ket(0),ket(0)) + kron(ket(1), ket(1)));
+% phiplus = phiplus * phiplus'
+% 
+% term_1 = kron(PartialTranspose(phiplus,2,[2 2]), eye(4)); % Hilbert spaces: Ai Aux Ao Bi
+% term_2 = kron(eye(2), decoder_choi); % Hilbert spaces: Ai Aux Ao Bi
+% 
+% Removing Aux:
+% W = PartialTrace(term_1 * term_2, 2,[2 2 2 2]);
+% 
+% [J,S ] = ccdc_complete([2 2 2 1 1 1], 'GR', 'primal', W);
+% % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % % 
+ 
 
 
 
