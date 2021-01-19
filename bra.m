5e39e1d7947440d95d81b597581f6b11ad44e09c
Marcello <mnerygvb@gmail.com>
2021-01-18 21:12:28 -0300

    Removed old versions of codes

--- bra.m
+++
@@ -1,9 +0,0 @@
-function braout = bra(i,d)
-% Function for returning <i| of dimension d in the computational basis.
-% Standard dimension d = 2 (<0|, <1|)
-if nargin < 2
-    braout = ket(i)';
-else
-   braout = ket(i,d)';    
-end
-
--- ketbra.m
+++
@@ -1,8 +0,0 @@
-function ketbraout = ketbra(i,j,d)
-% Function for returning |i><j| in the computational basis. 
-% Standard dimension = 2 (|0><0|, |0><1|, |1><0|, |1><1|)
-if nargin < 3
-    ketbraout = ket(i) * bra(j);
-else
-    ketbraout = ket(i,d) * bra(j,d);
-end
