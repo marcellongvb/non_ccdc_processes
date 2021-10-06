a11d78d43cfac3d3250550652db1398bea0f4918
Marco Túlio Quintino <mtcq.mm@gmail.com>
2021-10-06 22:13:37 +0200

    Delete Codes directory

--- Codes/Bloch2Rho.m
+++
@@ -1,4 +0,0 @@
-function [Rho] = Bloch2Rho(BlochVector)
-
-    Rho =1/2*[1+BlochVector(3), BlochVector(1)-sqrt(-1)*BlochVector(2); BlochVector(1)+sqrt(-1)*BlochVector(2), 1-BlochVector(3)];
-end
--- Codes/NRandomPureStates.m
+++
@@ -1,9 +0,0 @@
-function [states] = NRandomPureStates(d, N)
-% This function returns a set of random pure states with dimension d
-states=zeros(d,d,N);
-for i=1:N
-psi = RandomStateVector(d);
-states(:,:,i) = psi*psi';
-end
-
-end
